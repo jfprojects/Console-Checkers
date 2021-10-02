1e3d406e116e8a981280917a54f04a3a941d8d86
unknown <jeryfan@hotmail.com>
2021-10-01 23:50:43 -0700

    Modified .findMoves() to incorporate game logic into finding moves.

--- Code/King.cpp
+++ Code/King.cpp
@@ -18,23 +18,38 @@ std::vector<Coordinate> King::findMoves(Coordinate selection, const Board& board
 	int x = selection.x_;
 	int y = selection.y_;
 
+	int board_size = board.getSize();
+	const auto& board_array = board.getBoardArray();
+
 	// Define the move deltas
 	std::vector<int> x_deltas;
 	std::vector<int> y_deltas;
-	if (player_ == 0) {
-		x_deltas.insert(x_deltas.end(), { -1, 1, -2, 2, -1, 1, -2, 2 });
-		y_deltas.insert(y_deltas.end(), { 1, 1, 2, 2, -1, -1, -2, -2 });
-	}
-	else {
-		x_deltas.insert(x_deltas.end(), { -1, 1, -2, 2 });
-		y_deltas.insert(y_deltas.end(), { -1, -1, -2, -2 });
-	}
+
+	x_deltas.insert(x_deltas.end(), { 1, 1, 2, 2, -1, -1, -2, -2 });
+	y_deltas.insert(y_deltas.end(), { 1, -1, 2, -2, 1, -1, 2, -2 });
 
 	// Find moves
 	std::vector<Coordinate> moves;
 	for (unsigned i = 0; i < x_deltas.size(); i++) {
-		Coordinate move = Coordinate(x + x_deltas[i], y + y_deltas[i]);
-		moves.push_back(move);
+		int x_to = x + x_deltas[i];
+		int y_to = y + y_deltas[i];
+		Coordinate move = Coordinate(x_to, y_to);
+		if (board.checkOnBoard(move)) {
+			if (std::abs(x_deltas[i]) == 1) {
+				if (!board_array[x_to][y_to]) {
+					moves.push_back(move);
+				}
+			}
+			else {
+				int x_mid = (x + x_to) / 2;
+				int y_mid = (y + y_to) / 2;
+				if (board_array[x_mid][y_mid]) {
+					if (board_array[x_mid][y_mid]->getPlayer() != player_) {
+						moves.push_back(move);
+					}
+				}
+			}
+		}
 	}
 
 	return moves;
